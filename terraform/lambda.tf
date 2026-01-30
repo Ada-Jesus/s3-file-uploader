e535dff118aeccad1e7264c422444546076cf35f
Ada-Jesus <gracechieke@gmail.com>
2026-01-30 13:48:48 +0000

    change the lambda.tf role add .arn

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,9 @@
 resource "aws_lambda_function" "file_processing_function" {
-  function_name     = var.lambda_function_name
-  role              = aws_iam_role.lambda_exec_role
-  handler           = var.lambda_handler
-  runtime           = var.lambda_runtime
-  filename          = var.lambda_source_file
-  source_code_hash  = filebase64sha256(var.lambda_source_file)
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = var.lambda_handler
+  runtime          = var.lambda_runtime
+  filename         = var.lambda_source_file
+  source_code_hash = filebase64sha256(var.lambda_source_file)
 }
+
