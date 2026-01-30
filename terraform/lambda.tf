1a26a8414ea01a05b087a9771551df3b68c41f55
Ada-Jesus <gracechieke@gmail.com>
2026-01-30 13:46:28 +0000

    change the lambda.tf name

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,11 +1,8 @@
-# Function using the layer
-resource "aws_lambda_function" "example" {
-  function_name = var.lambda_function_name
-  role          = aws_iam_role.lambda_exec_role
-  handler       = var.lambda_handler
-  runtime       = var.lambda_runtime
-
-  filename      = var.lambda_source_file
-  source_code_hash = filebase64sha256(var.lambda_source_file)
- # source_code_hash = "placeholder" 
+resource "aws_lambda_function" "file_processing_function" {
+  function_name     = var.lambda_function_name
+  role              = aws_iam_role.lambda_exec_role
+  handler           = var.lambda_handler
+  runtime           = var.lambda_runtime
+  filename          = var.lambda_source_file
+  source_code_hash  = filebase64sha256(var.lambda_source_file)
 }
