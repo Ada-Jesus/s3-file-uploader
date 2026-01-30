3aa60f0c64ef3cc7bc744d4fcffca7b738561b55
Ada-Jesus <gracechieke@gmail.com>
2026-01-30 13:40:16 +0000

    updated terraform login

---
+++ terraform/iam-role.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.s3_bucket_prefix}-lambda-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  type        = string
+  description = "AWS region for the deployment"
+  default     = "us-east-1"
+}
+
+variable "lambda_handler" {
+  type        = string
+  description = "Lambda handler function name"
+  default     = "lambda_function.lambda_handler"
+}
+
+variable "lambda_runtime" {
+  type        = string
+  description = "Lambda runtime environment"
+  default     = "python3.11"
+}
+
+variable "lambda_source_file" {
+  type        = string
+  description = "Path to the Lambda source code zip file"
+  default     = "./lambda_function_payload.zip"
+}
+
+variable "lambda_function_name" {
+  type        = string
+  description = "Name of the Lambda function for processing S3 files"
+  default     = "file-processing-function"
+}
+
+variable  "s3_bucket_prefix" {
+  type        = string
+  description = "the prefix name of the S3 buckets for uploads and proceesed file"
+  default     = "cloudcore-s3-file"
+}
